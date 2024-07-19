48f718ef2ee82e1b443b5592697603f09ef8efd7
Faiz Syed <88630617+faizsyed999@users.noreply.github.com>
2024-07-19 23:34:09 +0530

    c

--- src/app/routes/home/EntryPage.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import HomePage from "../../components/HomePage";
-
-export default function Home() : ReactNode  {
-    return <HomePage></HomePage>;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,25 @@
 import React, { ReactNode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, useRoutes } from 'react-router-dom'
-import HomePage from './app/routes/home/EntryPage'
+import Home from './app/routes/Home'
+import HomePage from './app/routes/HomePage'
+import Login from './app/routes/Login'
 
-const browserRouter  = createBrowserRouter([{
-  path: "/",
-  element : <HomePage></HomePage>,
-}]);
-const  element = <h1>yolo</h1>;
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>
+  }, {
+    path: "/login",
+    element: <Login></Login>
+  }, {
+    path: "/home",
+    element: <Home></Home>
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
   <React.StrictMode>
     <RouterProvider router={browserRouter}></RouterProvider>
   </React.StrictMode>
